6e00b775bc53fd3b7129f16445da97b97a2a2856
Dipak <dipakdh12345@gmail.com>
2024-07-29 21:32:25 +0530

    Ensuring the code is well-structured and properly commented.

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,21 +1,3 @@
-// import TaskItem from './TaskItem';
-
-// export default function TaskList({ tasks, toggleTask, setEditTaskId }) {
-//   return (
-//     <div>
-//       {tasks.map(task => (
-//         <TaskItem
-//           key={task.id}
-//           task={task}
-//           toggleTask={toggleTask}
-//           setEditTaskId={setEditTaskId} // Pass the setEditTaskId function
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
-
 // components/TaskList.js
 import TaskItem from './TaskItem';
 
